6917d84cdd9eacebef2b418eb9e5872c9d4463c6
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-03-02 11:30:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EnergyRoad
+questa applicazione punta alla gestione e monitoraggio di una rete di fonti energetiche e all'offerta di servizi e prodotti per privati
---
+++ src/test/java/gabrielebelluco/EnergyRoad/EnergyRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrielebelluco.EnergyRoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
