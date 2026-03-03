0c1adbb543b6cee1543658d850738fd084a61810
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-03-03 12:48:56 +0100

    aggiuntaRole Service
    in questo commit è stato aggiunto la base di Role Service

---
+++ src/main/java/gabrielebelluco/EnergyRoad/services/RoleService.java
@@ -0,0 +1,53 @@
+package gabrielebelluco.EnergyRoad.services;
+
+import gabrielebelluco.EnergyRoad.entities.Role;
+import gabrielebelluco.EnergyRoad.exceptions.NotFoundException;
+import gabrielebelluco.EnergyRoad.payloads.RoleDTO;
+import gabrielebelluco.EnergyRoad.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public Page<Role> getAll(Pageable pageable) {
+        return roleRepository.findAll(pageable);
+    }
+
+    public Role findById(UUID ruoloId) {
+        return roleRepository.findById(ruoloId).orElseThrow(()
+                -> new NotFoundException("Ruolo non trovato con id: " + ruoloId));
+    }
+
+    public Role saveRuolo(RoleDTO payload) {
+        if (roleRepository.existsByRoleType(payload.getRoleType()))
+            throw new IllegalArgumentException("Il ruolo " + payload.getRoleType() + " esiste già");
+        Role savedRole = new Role();
+        savedRole.setRoleType(payload.getRoleType());
+        System.out.println("è stato salvato con successo ruolo di: " + payload.getRoleType());
+        return roleRepository.save(savedRole);
+    }
+
+    public Role findByIdAndUpdate(UUID ruoloId, RoleDTO payload) {
+        Role found = this.findById(ruoloId);
+        found.setRoleType(payload.getRoleType());
+        System.out.println("è stato modificato con successo il ruolo con id :" + ruoloId);
+        return roleRepository.save(found);
+    }
+
+    public void findByIdAndDelete(UUID ruoloId) {
+        Role found = this.findById(ruoloId);
+        roleRepository.delete(found);
+        System.out.println("è stato eliminato con successo il ruolo con id " + ruoloId);
+    }
+}
