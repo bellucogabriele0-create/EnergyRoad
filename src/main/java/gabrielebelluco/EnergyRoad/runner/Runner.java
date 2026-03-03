f5225af4fed2637072fe402ac2b1a476ab641c8f
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-03-03 21:31:03 +0100

    db runner
    è stato aggiunto il runner per popolare il db dei ruoli all'avvio

---
+++ src/main/java/gabrielebelluco/EnergyRoad/runner/Runner.java
@@ -0,0 +1,31 @@
+package gabrielebelluco.EnergyRoad.runner;
+
+
+import gabrielebelluco.EnergyRoad.entities.Role;
+import gabrielebelluco.EnergyRoad.enums.RoleType;
+import gabrielebelluco.EnergyRoad.repositories.RoleRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class Runner {
+
+    @Bean
+    public ApplicationRunner loadRoles(RoleRepository roleRepository) {
+        return args -> {
+
+            for (RoleType roleType : RoleType.values()) {
+
+                if (!roleRepository.existsByRoleType(roleType)) {
+                    Role role = new Role();
+                    role.setRoleType(roleType);
+                    roleRepository.save(role);
+                }
+
+            }
+
+        };
+    }
+}
--- src/main/java/gabrielebelluco/EnergyRoad/services/RoleService.java
+++ src/main/java/gabrielebelluco/EnergyRoad/services/RoleService.java
@@ -1,13 +1,16 @@
 package gabrielebelluco.EnergyRoad.services;
 
 import gabrielebelluco.EnergyRoad.entities.Role;
+import gabrielebelluco.EnergyRoad.enums.RoleType;
 import gabrielebelluco.EnergyRoad.exceptions.NotFoundException;
 import gabrielebelluco.EnergyRoad.payloads.RoleDTO;
 import gabrielebelluco.EnergyRoad.repositories.RoleRepository;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -48,6 +51,21 @@ public class RoleService {
     public void findByIdAndDelete(UUID ruoloId) {
         Role found = this.findById(ruoloId);
         roleRepository.delete(found);
-        System.out.println("è stato eliminato con successo il ruolo con id " + ruoloId);
+    }
+
+    @Transactional
+    public Role addRole(RoleDTO payload) throws BadRequestException {
+        // Verifica che il ruolo non esista già
+        if (roleRepository.existsByRoleType(payload.getRoleType())) {
+            throw new BadRequestException("è già esiste il ruolo: " + payload.getRoleType());
+        }
+        Role newRuolo = new Role(payload.getRoleType());
+        Role saved = this.roleRepository.save(newRuolo);
+        System.out.println("Ruolo creato: " + saved.getRoleType());
+        return saved;
+    }
+
+    public boolean existsByRoleType(RoleType roleType) {
+        return roleRepository.existsByRoleType(roleType);
     }
 }
