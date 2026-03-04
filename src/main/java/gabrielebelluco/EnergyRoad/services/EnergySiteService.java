91700ddcc587efa77dd043669fd3164118b39640
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-03-04 18:56:43 +0100

    service
    aggiunta get by id

--- src/main/java/gabrielebelluco/EnergyRoad/services/EnergySiteService.java
+++ src/main/java/gabrielebelluco/EnergyRoad/services/EnergySiteService.java
@@ -1,10 +1,12 @@
 package gabrielebelluco.EnergyRoad.services;
 
 import gabrielebelluco.EnergyRoad.entities.EnergySite;
+import gabrielebelluco.EnergyRoad.exceptions.NotFoundException;
 import gabrielebelluco.EnergyRoad.repositories.EnergySiteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EnergySiteService {
@@ -16,5 +18,9 @@ public class EnergySiteService {
 
     public List<EnergySite> getAll() {
         return energySiteRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    public EnergySite getById(UUID id) {
+        return energySiteRepository.findById(id).orElseThrow(() -> new NotFoundException("sito non trovato"));
     }
 }
