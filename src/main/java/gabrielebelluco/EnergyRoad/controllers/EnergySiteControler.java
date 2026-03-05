f180190b90d400a32dd3f112949c9f76b72954b8
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-03-05 11:18:54 +0100

    controller
    get all

--- src/main/java/gabrielebelluco/EnergyRoad/controllers/EnergySiteControler.java
+++ src/main/java/gabrielebelluco/EnergyRoad/controllers/EnergySiteControler.java
@@ -6,8 +6,11 @@ import gabrielebelluco.EnergyRoad.payloads.EnergySiteCreateDTO;
 import gabrielebelluco.EnergyRoad.services.EnergySiteService;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/site")
@@ -18,8 +21,13 @@ public class EnergySiteControler {
         this.energySiteService = energySiteService;
     }
 
+    @GetMapping
+    public List<EnergySite> getAll() {
+        return energySiteService.getAll();
+    }
+
     @PostMapping
-    //@PreAuthorize("hasAnyAuthority('ADMIN','FOUNDER')")
+    @PreAuthorize("hasAnyAuthority('ADMIN','FOUNDER')")
     @ResponseStatus(HttpStatus.CREATED)
     public EnergySite create(@RequestBody @Validated EnergySiteCreateDTO dto) throws BadRequestException {
         return energySiteService.create(dto);
